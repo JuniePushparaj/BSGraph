d975a8dcce07ffa7f8a011a215ba1007d4094579
JuniePushparaj <erjuniebgt@gmail.com>
2019-12-31 08:47:22 +0530

    Initital commit 2

--- BSGraph.py
+++ BSGraph.py
@@ -1,26 +1,50 @@
 class BSGraph:
     def __init__(self):
-        self.ActMov = [];
-        self.edges = [];
+        self.ActMov = []
+        self.edges = []
 
-    def readActMovfile(self, inputfile):
-        inputFileData = open(inputfile,'r');
-        for line in inputFileData:
-            actmovList = line.split('/');
-            if len(actmovList)>0 :
-                movie = actmovList[0];
-                for i in range(1, len(actmovList)):
-                    createNodeEdge(movie,actmovList[i]);
+    def readActMovfile(self, inputFile):
+        try:
+            inputFileData = open(inputFile, 'r')
+            for line in inputFileData:
+                actmovList = line.strip().split('/')
+                if len(actmovList) > 0:
+                    movie = "mov_" + actmovList[0].strip()
+                    for i in range(1, len(actmovList)):
+                        self.createNodeEdge(movie, "act_" + actmovList[i].strip())
+        except Exception as e:
+            self.writeOutput("Exception occured:" + str(e))
+        finally:
+            inputFileData.close()
     
+    def writeOutput(self, data):
+        header = ''
+        footer = ''
+        try:
+            data = hr + '\n' + data + '\n' + hr + '\n'
+            outputFile = open(filePath,'a+')
+            print >> outputFile, data
+            print(data)
+        except Exception as e:
+            print("Exception occured:" + str(e))
+        finally:
+            outputFile.close()
+
     def createNodeEdge(self, movie, actor):
-        movIndex = None;
-        actIndex = None;
+        movIndex = None
+        actIndex = None
         if movie not in self.ActMov:
-            self.ActMov.append(movie);
-            movIndex = len(self.ActMov)-1;
+            self.ActMov.append(movie)
+            movIndex = len(self.ActMov)-1
         if actor not in self.ActMov:
-            self.ActMov.append(actor);
-            actIndex = len(self.ActMov)-1;
-        
-        if (movIndex is not None && actIndex is not None):
-            self.edges[movIndex]
+            self.ActMov.append(actor)
+            actIndex = len(self.ActMov)-1
+        if (movIndex is not None and actIndex is not None):
+            self.edges[movIndex][actIndex] = 1
+            self.edges[actIndex][movIndex] = 1
+            self.edges[movIndex][movIndex] = 0
+            self.edges[actIndex][actIndex] = 0
+    
+    def adjacencyMatrix():
+        for s in a:
+            print(*s)
---
+++ main.py
@@ -0,0 +1,6 @@
+from BSGraph import BSGraph
+import json
+
+
+bsgraph = BSGraph()
+bsgraph.readActMovfile(r".\InputFiles\inputPS2.txt")
